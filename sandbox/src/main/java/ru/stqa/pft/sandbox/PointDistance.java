33587b58306cadcba5260581261744e83131fd84
kvlazarenko <kv.lazarenko@gmail.com>
2022-10-19 20:03:46 +0300

    Дистанция между точками (функция)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -4,16 +4,19 @@ public class PointDistance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(5, 5);
+    Point p1 = new Point(77, 5);
     Point p2 = new Point(4, 6);
 
 
-    System.out.println("Расстояния между точками  = " + distance(p1, p2));
+    System.out.println("Координаты точки Р1 (x,y) - " + p1.x + " , " + p1.y);
+    System.out.println("Координаты точки Р2 (x,y) - " + p2.x + " , " + p2.y);
+    System.out.println("Расстояния между точками на плоскости = " + distance(p1, p2));
 
   }
 
   public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.getY() - p1.getY()) * (p2.getY() - p1.getY()) + (p2.getX() - p1.getX()) * (p2.getX() - p1.getX()));
+      return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+
   }
 }
 
