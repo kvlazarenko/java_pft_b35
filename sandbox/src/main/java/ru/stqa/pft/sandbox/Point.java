d6528117e75db5a4db1c80880fee54e85666f76d
kvlazarenko <kv.lazarenko@gmail.com>
2022-10-20 13:52:41 +0300

    Дистанция между точками (метод)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,14 +1,19 @@
 package ru.stqa.pft.sandbox;
 
+
 public class Point {
   public double x;
   public double y;
 
   public Point(double x, double y) {
-
     this.x = x;
     this.y = y;
   }
+
+
+ public double distance( Point p1) {
+  return Math.sqrt((this.y - p1.y) * (this.y - p1.y) + (this.x - p1.x) * (this.x - p1.x));
+ }
 
 }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -1,21 +1,17 @@
 package ru.stqa.pft.sandbox;
 
+
 public class PointDistance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(77, 5);
-    Point p2 = new Point(4, 6);
+    Point p1 = new Point(7.0, 5.5);
+    Point p2 = new Point(8,5);
 
 
     System.out.println("Координаты точки Р1 (x,y) - " + p1.x + " , " + p1.y);
     System.out.println("Координаты точки Р2 (x,y) - " + p2.x + " , " + p2.y);
-    System.out.println("Расстояния между точками на плоскости = " + distance(p1, p2));
-
-  }
-
-  public static double distance(Point p1, Point p2) {
-      return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+    System.out.println("Расстояния между точками на плоскости = " + p2.distance(p1));
 
   }
 }
