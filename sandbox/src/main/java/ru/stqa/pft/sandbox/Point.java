e1953d38521be376a7b6a1db658ec1b05d92f266
kvlazarenko <kv.lazarenko@gmail.com>
2022-10-24 12:49:23 +0300

    Дистанция между точками (метод v3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,31 +1,3 @@
-//package ru.stqa.pft.sandbox;
-//
-//
-//public class Point {
-//  public double x1;
-//  public double y1;
-//  public int x2;
-//  public int y2;
-//
-//  public Point(double x1, double y1) {
-//
-//    this.x1 = x1;
-//    this.y1 = y1;
-//
-//  }
-//
-//  public Point(int x2, int y2) {
-//
-//    this.x2 = x2;
-//    this.y2 = y2;
-//  }
-//
-//
-//  public double distance(Point p2) {
-//    return Math.sqrt(((p2.y2 - this.y1) * (p2.y2 - this.y1)) + ((p2.x2 - this.x1) * (p2.x2 - this.x1)));
-//  }
-//
-//}
 
 package ru.stqa.pft.sandbox;
 
@@ -42,17 +14,20 @@ public class Point {
     return x;
   }
 
-//  public void setX(double x) {
-//    this.x = x;
-//  }
+  public void setX(double x) {
+    this.x = x;
+  }
 
   public double getY() {
     return y;
   }
 
-//  public void setY(double y) {
-//    this.y = y;
-//  }
+  public void setY(double y) {
+    this.y = y;
+  }
+  public double distance(Point point) {
+    return Math.sqrt((point.getX()-this.getX())*(point.getX()-this.getX())+(point.getY()-this.getY())*(point.getY()-this.getY()));
+  }
 }
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -2,32 +2,15 @@
 package ru.stqa.pft.sandbox;
 
 public class PointDistance {
-
   public static void main(String[] args) {
 
     Point p1 = new Point(2, 8);
     Point p2 = new Point(1, 2);
 
 
-    System.out.println("Расстояния между точками  = " + distance(p1, p2));
+    System.out.println("Координаты точки Р1 (x,y) - " + p1.getX() + " , " + p1.getY());
+    System.out.println("Координаты точки Р2 (x,y) - " + p2.getX() + " , " + p2.getY());
+    System.out.println("Расстояния между точками на плоскости = " + p1.distance(p2));
 
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.getY() - p1.getY()) * (p2.getY() - p1.getY()) + (p2.getX() - p1.getX()) * (p2.getX() - p1.getX()));
   }
 }
-
-//public class PointDistance {
-//  public static void main(String[] args) {
-//
-//    Point p1 = new Point(4.0, 8.0);
-//    Point p2 = new Point(6, 2);
-//
-//
-//    System.out.println("Координаты точки Р1 (x,y) - " + p1.x1 + " , " + p1.y1);
-//    System.out.println("Координаты точки Р2 (x,y) - " + p2.x2 + " , " + p2.y2);
-//    System.out.println("Расстояния между точками на плоскости = " + p1.distance(p2));
-//
-//  }
-//}
