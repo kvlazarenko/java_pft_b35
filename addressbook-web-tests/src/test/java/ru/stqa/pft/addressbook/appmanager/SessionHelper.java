95ab4f1aeec82b5ed4987ff650ca132f40ae8efc
kvlazarenko <kv.lazarenko@gmail.com>
2022-10-26 13:51:09 +0300

    Создаём общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,22 +3,18 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
+public class SessionHelper extends HelperBase {
 
-  private WebDriver wd;
 
   public SessionHelper(WebDriver wd) {
 
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String username, String password) {
     wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
     wd.findElement(By.id("LoginForm")).submit();
   }
 }
