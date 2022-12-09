428c33ac282eac5355537b908219fe0c4e145d9e
kvlazarenko <kv.lazarenko@gmail.com>
2022-12-09 15:33:53 +0300

    Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,8 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysql';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_crypto_master_salt = 'spLp0yx+bLfP3an1QycDhUDLDw4GjM7OzB2RtbvI3jQ=';
+$g_signup_use_captcha = OFF;
